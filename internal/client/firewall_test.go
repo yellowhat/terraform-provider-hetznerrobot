487e2a42d87aa2b01ae8533176b4f0734abe6906
yellowhat <1692490+yellowhat@users.noreply.github.com>
2025-08-26 15:52:37 +0200

    asd

--- internal/client/firewall_test.go
+++ internal/client/firewall_test.go
@@ -7,115 +7,121 @@ import (
 	"github.com/yellowhat/terraform-provider-hetznerrobot/internal/client"
 )
 
+//nolint:gochecknoglobals
+var testFirewall = client.Firewall{
+	IP:                       "1.2.3.4",
+	WhitelistHetznerServices: true,
+	Status:                   "active",
+	Rules: client.FirewallRules{
+		//exhaustruct:ignore
+		Input: []client.FirewallRule{
+			{
+				Name:     "allow-ssh",
+				SrcIP:    "0.0.0.0/0",
+				DstPort:  "22",
+				Protocol: "tcp",
+				Action:   "accept",
+			},
+			{
+				Name:     "allow-http",
+				SrcIP:    "0.0.0.0/0",
+				DstPort:  "80",
+				Protocol: "tcp",
+				Action:   "accept",
+			},
+		},
+	},
+}
+
 func TestGetFirewall(t *testing.T) {
 	t.Parallel()
 
-	tests := []struct {
-		name       string
-		IP         string
-		wantHOS    bool
-		wantStatus string
-		wantRules  []client.FirewallRule
-	}{
-		{
-			name:       "successful firewall retrieval",
-			IP:         "1.2.3.4",
-			wantHOS:    true,
-			wantStatus: "active",
-			//exhaustruct:ignore
-			wantRules: []client.FirewallRule{
-				{
-					Name:     "allow-ssh",
-					SrcIP:    "0.0.0.0/0",
-					DstPort:  "22",
-					Protocol: "tcp",
-					Action:   "accept",
-				},
-				{
-					Name:     "allow-http",
-					SrcIP:    "0.0.0.0/0",
-					DstPort:  "80",
-					Protocol: "tcp",
-					Action:   "accept",
-				},
-			},
-		},
+	server := mockServer()
+	defer server.Close()
+
+	client := client.New(&client.ProviderConfig{
+		Username: testUsername,
+		Password: testPassword,
+		BaseURL:  server.URL,
+	})
+
+	firewall, err := client.GetFirewall(context.Background(), testFirewall.IP)
+	if err != nil {
+		t.Errorf("GetFirewall() error: %v", err)
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			t.Parallel()
+	if testFirewall.IP != firewall.IP {
+		t.Errorf("IP: want %v, got %v", testFirewall.IP, firewall.IP)
+	}
 
-			server := mockServer()
-			defer server.Close()
+	if testFirewall.WhitelistHetznerServices != firewall.WhitelistHetznerServices {
+		t.Errorf(
+			"WhitelistHetznerServices: want %t, got %t",
+			testFirewall.WhitelistHetznerServices,
+			firewall.WhitelistHetznerServices,
+		)
+	}
 
-			client := client.New(&client.ProviderConfig{
-				Username: testUsername,
-				Password: testPassword,
-				BaseURL:  server.URL,
-			})
+	if testFirewall.Status != firewall.Status {
+		t.Errorf("Status: want %v, got %v", testFirewall.Status, firewall.Status)
+	}
 
-			firewall, err := client.GetFirewall(context.Background(), test.IP)
-			if err != nil {
-				t.Errorf("GetFirewall() error: %v", err)
-			}
+	if len(testFirewall.Rules.Input) != len(firewall.Rules.Input) {
+		t.Errorf(
+			"Rules length: want %d, got %d",
+			len(testFirewall.Rules.Input),
+			len(firewall.Rules.Input),
+		)
+	}
 
-			if test.IP != firewall.IP {
-				t.Errorf("IP: want %v, got %v", test.IP, firewall.IP)
-			}
+	for i, wantRule := range testFirewall.Rules.Input {
+		gotRule := firewall.Rules.Input[i]
+		if wantRule.Name != gotRule.Name {
+			t.Errorf("Rule[%d] Name: want %v, got %v", i, wantRule.Name, gotRule.Name)
+		}
 
-			if test.wantHOS != firewall.WhitelistHetznerServices {
-				t.Errorf(
-					"WhitelistHetznerServices: want %t, got %t",
-					test.wantHOS,
-					firewall.WhitelistHetznerServices,
-				)
-			}
+		if wantRule.SrcIP != gotRule.SrcIP {
+			t.Errorf("Rule[%d] SrcIP: want %v, got %v", i, wantRule.SrcIP, gotRule.SrcIP)
+		}
 
-			if test.wantStatus != firewall.Status {
-				t.Errorf("Status: want %v, got %v", test.wantStatus, firewall.Status)
-			}
+		if wantRule.DstPort != gotRule.DstPort {
+			t.Errorf(
+				"Rule[%d] DstPort: want %v, got %v",
+				i,
+				wantRule.DstPort,
+				gotRule.DstPort,
+			)
+		}
 
-			if len(test.wantRules) != len(firewall.Rules.Input) {
-				t.Errorf(
-					"Rules length: want %d, got %d",
-					len(test.wantRules),
-					len(firewall.Rules.Input),
-				)
-			}
+		if wantRule.Protocol != gotRule.Protocol {
+			t.Errorf(
+				"Rule[%d] Protocol: want %v, got %v",
+				i,
+				wantRule.Protocol,
+				gotRule.Protocol,
+			)
+		}
 
-			for i, wantRule := range test.wantRules {
-				gotRule := firewall.Rules.Input[i]
-				if wantRule.Name != gotRule.Name {
-					t.Errorf("Rule[%d] Name: want %v, got %v", i, wantRule.Name, gotRule.Name)
-				}
+		if wantRule.Action != gotRule.Action {
+			t.Errorf("Rule[%d] Action: want %v, got %v", i, wantRule.Action, gotRule.Action)
+		}
+	}
+}
 
-				if wantRule.SrcIP != gotRule.SrcIP {
-					t.Errorf("Rule[%d] SrcIP: want %v, got %v", i, wantRule.SrcIP, gotRule.SrcIP)
-				}
+func TestSetFirewall(t *testing.T) {
+	t.Parallel()
 
-				if wantRule.DstPort != gotRule.DstPort {
-					t.Errorf(
-						"Rule[%d] DstPort: want %v, got %v",
-						i,
-						wantRule.DstPort,
-						gotRule.DstPort,
-					)
-				}
+	server := mockServer()
+	defer server.Close()
 
-				if wantRule.Protocol != gotRule.Protocol {
-					t.Errorf(
-						"Rule[%d] Protocol: want %v, got %v",
-						i,
-						wantRule.Protocol,
-						gotRule.Protocol,
-					)
-				}
+	client := client.New(&client.ProviderConfig{
+		Username: testUsername,
+		Password: testPassword,
+		BaseURL:  server.URL,
+	})
 
-				if wantRule.Action != gotRule.Action {
-					t.Errorf("Rule[%d] Action: want %v, got %v", i, wantRule.Action, gotRule.Action)
-				}
-			}
-		})
+	err := client.SetFirewall(context.Background(), testFirewall)
+	if err != nil {
+		t.Errorf("SetFirewall() error: %v", err)
 	}
 }
